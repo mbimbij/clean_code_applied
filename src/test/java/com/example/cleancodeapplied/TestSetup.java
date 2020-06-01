01af39851c7ee0ae9342ea9f3db6338b5f984c04
joseph m'bimbi-bene <Joseph.Mbimbi-Bene.ext@ing.com>
2020-06-01 08:36:40 +0200

    wip front-end

--- src/test/java/com/example/cleancodeapplied/TestSetup.java
+++ src/test/java/com/example/cleancodeapplied/TestSetup.java
@@ -4,6 +4,10 @@ import com.example.cleancodeapplied.doubles.InMemoryCodecastGateway;
 import com.example.cleancodeapplied.doubles.InMemoryLicenseGateway;
 import com.example.cleancodeapplied.doubles.InMemoryUserGateway;
 
+import java.time.ZonedDateTime;
+
+import static com.example.cleancodeapplied.License.Type.VIEW;
+
 public class TestSetup {
 
     public static void setupContext() {
@@ -11,5 +15,20 @@ public class TestSetup {
         Context.licenseGateway = new InMemoryLicenseGateway();
         Context.userGateway = new InMemoryUserGateway();
         Context.gateKeeper = new GateKeeper();
+    }
+
+    public static void setupSampleData() {
+        setupContext();
+
+        User bob = new User("Bob");
+        User micah = new User("Micah");
+
+        Codecast episode1 = new Codecast("Episode 1 - The Beginning", ZonedDateTime.now().minusDays(1));
+        Codecast episode2 = new Codecast("Episode 2 - The Continuation", ZonedDateTime.now());
+
+        License bobEpisode1 = new License(VIEW, bob, episode1);
+        License bobEpisode2 = new License(VIEW, bob, episode2);
+
+        Context.userGateway.save(bob);
     }
 }
