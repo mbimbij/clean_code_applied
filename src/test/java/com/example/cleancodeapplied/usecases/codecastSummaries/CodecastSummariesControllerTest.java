991d0e8376e5562208ca86874c131ed2db4f0f93
joseph m'bimbi-bene <Joseph.Mbimbi-Bene.ext@ing.com>
2020-06-05 01:43:06 +0200

    wip

--- src/test/java/com/example/cleancodeapplied/usecases/codecastSummaries/CodecastSummariesControllerTest.java
+++ src/test/java/com/example/cleancodeapplied/usecases/codecastSummaries/CodecastSummariesControllerTest.java
@@ -10,17 +10,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CodecastSummariesControllerTest {
     @Test
-    void testFrontPage() {
+    void testInputBoundaryInvocation() {
         TestSetup.setupSampleData();
 
         CodecastSummaryInputBoundarySpy codecastSummaryInputBoundary = new CodecastSummaryInputBoundarySpy();
         CodecastSummariesController controller = new CodecastSummariesController(codecastSummaryInputBoundary);
 
-        ParsedRequest request = new ParsedRequest("GET","/");
+        ParsedRequest request = new ParsedRequest("GET","blah");
         controller.handle(request);
 
         assertThat(codecastSummaryInputBoundary.summarizeCodecastsWasCalled).isTrue();
-        User bob = Context.userGateway.findUserByName("Bob");
-        assertThat(codecastSummaryInputBoundary.requestedUser.getId()).isEqualTo(bob.getId());
+        User loggedInUser = Context.userGateway.findUserByName("Bob");
+        assertThat(codecastSummaryInputBoundary.requestedUser.getId()).isEqualTo(loggedInUser.getId());
+        CodecastSummaryOutputBoundary outputBoundary = codecastSummaryInputBoundary.outputBoundary;
+        assertThat(outputBoundary).isNotNull();
     }
 }
